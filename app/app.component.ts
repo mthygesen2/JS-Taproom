4e12972314f47c01e644a7ae128bf29836bf4158
Mao Thygesen <maothygesen@gmail.com>
2016-04-28 16:37:06 -0700

    Add a paragraph under the title.

--- app/app.component.ts
+++ app/app.component.ts
@@ -9,6 +9,8 @@ import {Keg} from './keg.model';
   template: `
     <div class="container">
       <h1>Pint Me</h1>
+      <h3> Welcome to the Taproom. Go ahead and add a new keg to the keg list! Or if you are feeling thirsty, go ahead and buy yourself a pint!</h3>
+       <h4>Remember that the expensive pints are listed in a nice marigold color.</h4>
       <keg-list
         [kegList]="kegs"
         (onKegSelect)="kegWasSelected($event)">
